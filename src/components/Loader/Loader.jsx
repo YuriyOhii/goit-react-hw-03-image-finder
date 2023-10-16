06f1ac93a175c1161adfd69f4ad4b898584732da
YuriyOhii <uaogiy@gmail.com>
2023-10-16 09:35:16 +0300

    updates loader

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import PropTypes from 'prop-types'
-import {  } from './Loader.styled'
+// import PropTypes from 'prop-types'
+// import {  } from './Loader.styled'
 export const Loader = props => {
   return (
     <div>Loader</div>
   )
 }
 
-Loader.propTypes = {}
+// Loader.propTypes = {}
